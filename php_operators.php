eb4f8721946687e52ea112700b1121fec1c8a0b1
Prinsa Joshi <joshi.prinsa1234@gmail.com>
2024-07-22 16:11:48 +0545

    third commit

--- php_operators.php
+++ php_operators.php
@@ -31,63 +31,51 @@ echo "Exponential: $f <br>"; // Output: Exponential: 117649
 
 // Equal ('==')
 $g = ($a == $b); // true
-echo "Equal: ";
 var_dump($g); // Output: bool(true)
 
 // Strictly Equal ('===')
 $h = ($a === $b); // false
-echo "Strictly Equal: ";
 var_dump($h); // Output: bool(false)
 
 // Not Equal ('!=')
 $i = ($a != $b); // false
-echo "Not Equal: ";
 var_dump($i); // Output: bool(false)
 
 // Not Strictly Equal ('!==')
 $j = ($a !== $b); // true
-echo "Not Strictly Equal: ";
 var_dump($j); // Output: bool(true)
 
 // Greater Than ('>')
 $k = ($a > $b); // false
-echo "Greater Than: ";
 var_dump($k); // Output: bool(false)
 
 // Less Than ('<')
 $l = ($a < $b); // false
-echo "Less Than: ";
 var_dump($l); // Output: bool(false)
 
 // Greater Than or Equal To ('>=')
 $m = ($a >= $b); // true
-echo "Greater Than or Equal To: ";
 var_dump($m); // Output: bool(true)
 
 // Less Than or Equal To ('<=')
 $n = ($a <= $b); // true
-echo "Less Than or Equal To: ";
 var_dump($n); // Output: bool(true)
 
 #Logical Operators
 
 // AND ('&&' or 'and')
 $result1 = ($a > 5 && $a < 10); // true
-echo "AND: ";
 var_dump($result1); // Output: bool(true)
 
 // OR ('||' or 'or')
 $result2 = ($a > 5 || $b < 10); // true
-echo "OR: ";
 var_dump($result2); // Output: bool(true)
 
 // NOT ('!' or 'not')
 $result3 = !($a > 5); // false
-echo "NOT: ";
 var_dump($result3); // Output: bool(false)
 
 // XOR ('xor')
 $result4 = ($a > 5 xor $b < 10); // false
-echo "XOR: ";
 var_dump($result4); // Output: bool(false)
 ?>
