ca94d675230e8ed0dbd16507a318c3603791c8f2
Prinsa Joshi <joshi.prinsa1234@gmail.com>
2024-07-22 16:16:14 +0545

    fourth commit

--- php_continue_break.php
+++ php_continue_break.php
@@ -1,20 +1,40 @@
 <?php
-#php continue and break
+// PHP break and continue
 
-#php break
-for($i=0;$i<10;$i++){
-    if ($i==5){
-        break;  //exit loop when the $i=5
+// PHP break
+echo "<b>PHP Break:</b><br>";
+for ($i = 0; $i < 10; $i++) {
+    if ($i == 5) {
+        break;  // Exit the loop when $i equals 5
     }
-    echo "The number is: $i";
+    echo "The number is: $i<br>";
 }
+/* Output:
+The number is: 0
+The number is: 1
+The number is: 2
+The number is: 3
+The number is: 4
+*/
 
-#php continue
-for($i=0;$i<10;$i++){
-    if ($i==5){
-        continue;  //skip loop when the $i=5
+// PHP continue
+echo "<b>PHP Continue:</b><br>";
+for ($i = 0; $i < 10; $i++) {
+    if ($i == 5) {
+        continue;  // Skip the current iteration when $i equals 5
     }
-    echo "The number is: $i \n";
+    echo "The number is: $i<br>";
 }
+/* Output:
+The number is: 0
+The number is: 1
+The number is: 2
+The number is: 3
+The number is: 4
+The number is: 6
+The number is: 7
+The number is: 8
+The number is: 9
+*/
 
 ?>
